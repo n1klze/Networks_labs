b7504286c02a32def9c7c11103fab5a6c9e5123b
Nikita Melnikov <114857730+n1klze@users.noreply.github.com>
2023-11-06 05:39:23 +0700

    fix codestyle

--- lab2/client.py
+++ lab2/client.py
@@ -2,43 +2,50 @@ import argparse
 from os.path import basename, getsize
 import socket
 import struct
-import sys
 
 BUFSIZ = 4096
 
-class Client():
+
+class Client:
     def __init__(self, filename, address, port):
         self.filename = filename
         self.addr     = address
-        self. port    = port 
-    
+        self.port     = port
+
     def run(self):
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP) as sock:
+        with socket.socket(
+            socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP
+        ) as sock:
             sock.connect((self.addr, self.port))
             filename_len = len(basename(self.filename))
             filename     = basename(self.filename)
             file_size    = getsize(self.filename)
-            header = struct.pack("!h" + str(filename_len) + "sq" ,
-                                 filename_len, filename.encode(), file_size)
+            header = struct.pack(
+                "!h{}sq".format(str(filename_len)),
+                filename_len,
+                filename.encode(),
+                file_size,
+            )
             sock.send(header)
-            
+
             with open(self.filename, "rb") as file:
                 buffer = file.read(BUFSIZ)
                 while buffer:
                     sock.send(buffer)
                     buffer = file.read(BUFSIZ)
-            
+
 
 def create_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('filename', nargs='?')
-    parser.add_argument('address', nargs='?')
-    parser.add_argument('-p', '--port', default='6969', dest='port', type=int)
+    parser.add_argument("filename", nargs="?")
+    parser.add_argument("address", nargs="?")
+    parser.add_argument("-p", "--port", default="6969", dest="port", type=int)
     return parser
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     parser = create_parser()
-    args = parser.parse_args()
-    
+    args   = parser.parse_args()
+
     client = Client(args.filename, args.address, args.port)
     client.run()
--- lab2/server.py
+++ lab2/server.py
@@ -6,20 +6,21 @@ import time
 
 HOST     = "127.0.0.1"  # Standard loopback interface address (localhost)
 BUFSIZ   = 4096
-INTERVAL = 3 * 10**9    #3 sec.
+INTERVAL = 3 * 10**9  # 3 sec.
+
 
 class ClientHandler(Thread):
     def __init__(self, conn, addr):
         Thread.__init__(self)
         self.conn = conn
-        self.addr = addr[0] + ':' + str(addr[1])
+        self.addr = "{0}:{1}".format(addr[0], str(addr[1]))
 
     def run(self):
         filename_len = int.from_bytes(self.conn.recv(2))
         filename     = self.conn.recv(filename_len).decode()
         file_size    = int.from_bytes(self.conn.recv(8))
 
-        with open("uploads/" + basename(filename), 'wb') as file:
+        with open("uploads/" + basename(filename), "wb") as file:
             read_bytes, prev_read_bytes = 0, 0
             start_time = time.time_ns()
             last_time  = start_time
@@ -31,27 +32,55 @@ class ClientHandler(Thread):
                 current_time = time.time_ns()
 
                 if current_time - last_time > INTERVAL:
-                    curent_speed = (read_bytes - prev_read_bytes) / 1024 / 1024 / (current_time - last_time) * 10**9
-                    avg_speed    = read_bytes / 1024 / 1024 / (current_time - start_time) * 10**9
-                    print(self.addr, ':', "curr =", curent_speed, "Mb/s", ",", "avg =", avg_speed, "Mb/s")
+                    curent_speed = (
+                        (read_bytes - prev_read_bytes)
+                        / 1024
+                        / 1024
+                        / (current_time - last_time)
+                        * 10**9
+                    )
+                    avg_speed = (
+                        read_bytes / 1024 / 1024 / (current_time - start_time) * 10**9
+                    )
+                    print(
+                        self.addr,
+                        ":",
+                        "curr =",
+                        curent_speed,
+                        "Mb/s",
+                        ",",
+                        "avg =",
+                        avg_speed,
+                        "Mb/s",
+                    )
                     last_time = time.time_ns()
                     prev_read_bytes = read_bytes
 
             if 0 < time.time_ns() - start_time <= INTERVAL:
-                avg_speed = read_bytes / 1024 / 1024 / (time.time_ns() - start_time) * 10**9
-                print(self.addr, ':', "avg =", avg_speed, "Mb/s")
+                avg_speed = (
+                    read_bytes / 1024 / 1024 / (time.time_ns() - start_time) * 10**9
+                )
+                print(self.addr, ":", "avg =", avg_speed, "Mb/s")
 
-        print(self.addr, ':', 
-              "Success" if getsize("uploads/" + basename(filename)) == file_size 
-              else "Failure: received: " + str(read_bytes) + "expected: " + str(file_size))
+        print(
+            self.addr,
+            ":",
+            "Success"
+            if getsize("uploads/" + basename(filename)) == file_size
+            else "Failure: received: {0} expected: {1}".format(
+                str(read_bytes), str(file_size)
+            ),
+        )
 
 
-class Server():
+class Server:
     def __init__(self, port):
         self.port = port
 
     def start(self):
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP) as sock:
+        with socket.socket(
+            socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP
+        ) as sock:
             sock.bind((HOST, self.port))
             sock.listen()
             while True:
@@ -62,12 +91,13 @@ class Server():
 
 def create_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-p', '--port', default='6969', dest='port', type=int)
+    parser.add_argument("-p", "--port", default="6969", dest="port", type=int)
     return parser
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     parser = create_parser()
-    args = parser.parse_args()
+    args   = parser.parse_args()
 
     server = Server(args.port)
     server.start()
